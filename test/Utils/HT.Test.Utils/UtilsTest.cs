54e3bc3839af33f9697c67248288f80ad0e5501d
Rodrigo Lima <rodrigo_rossetti_lima@hotmail.com>
2024-09-17 23:00:01 -0300

    feat: incluindo projeto testes

---
+++ test/Utils/HT.Test.Utils/UtilsTest.cs
@@ -0,0 +1,13 @@
+﻿namespace HT.Test.Utils
+{
+    public static class UtilsTest
+    {
+        public static T GetRandomEnum<T>(Array values) where T : Enum
+        {
+            Random random = new();
+            var enumRetorno = (T)values.GetValue(random.Next(values.Length))!;
+
+            return enumRetorno;
+        }
+    }
+}
